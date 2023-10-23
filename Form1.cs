f9b99194b40430249edac7c0ee8cd40fe4a6312d
barbo7 <b.sapancil@gmail.com>
2023-10-23 03:50:44 +0300

    Geliştirmeler yapıldı

--- Form1.cs
+++ Form1.cs
@@ -24,34 +24,38 @@ namespace kelimeciniz
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
             Tuple<List<string>, List<string>> veriTuple = gs.Sayfa1Veri();
 
             string[] sutunA = veriTuple.Item1.ToArray();
             string[] sutunB = veriTuple.Item2.ToArray();
 
             listBox1.Items.AddRange(sutunA);
-            listBox1.Items.AddRange(sutunB);
+            listBox2.Items.AddRange(sutunB);
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             gs= new GoogleSheets();
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            //string[] arama = gs.GetBValueByKeyword(textBox1.Text).Split(',');
-            //foreach (var i in arama)
-                //listBox1.Items.Add(i.Trim());
-            
+            listBox2.Items.Clear();
+
+            string[] words = gs.KelimeAra(textBox1.Text).Item1.ToArray();
+            string[] kelimeler = gs.KelimeAra(textBox1.Text).Item2.ToArray();
+
+            listBox1.Items.AddRange(words);
+            listBox2.Items.AddRange(kelimeler);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //gs.EkleVeri(textBox1.Text, textBox2.Text);
+            gs.VeriEkle(textBox1.Text, textBox2.Text);
             MessageBox.Show("Ekleme başarılı.");
         }
     }
--- GoogleSheets.cs
+++ GoogleSheets.cs
@@ -1,5 +1,4 @@
 ﻿using Google.Apis.Auth.OAuth2;
-using System.Collections;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
@@ -7,11 +6,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
+using Google.Cloud.Translation.V2;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using static Google.Apis.Requests.BatchRequest;
 
 namespace kelimeciniz
 {
@@ -38,11 +37,17 @@ namespace kelimeciniz
         string sutun2 = "Sayfa1!B:B"; //hangi satırı yazmak istediğim.
         string VeriKumesiIng = "Veriler!A:A";
         string VeriKumesiTr = "Veriler!B:B";
+
+        SpreadsheetsResource.ValuesResource.AppendRequest verireq;
+        ValueRange body;
+        string range = "Sayfa1!A:B"; //verieklemeSatırı
+
+        static string myMemoryApiKey = "1fb0d0fab1c449d5df11";
+
+
+
         public GoogleSheets()
         {
-            
-
-
             using (var stream = new FileStream("C:\\Users\\as\\source\\repos\\kelimeciniz\\client_secret_950495088287-0n98o9v2m357pvrif86ffbhspb73dg0d.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
             {
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
@@ -71,23 +76,21 @@ namespace kelimeciniz
             SpreadsheetsResource.ValuesResource.GetRequest reqArama =
               service.Spreadsheets.Values.Get(spreadsheetId, VeriKumesiTr);
 
-
             responseSutunA = request1.Execute();
             responseSutunB = request2.Execute();
             responseSearchWord = reqSearch.Execute();
             responseAramaKelime = reqArama.Execute();
 
-
             sutunAVeri = responseSutunA.Values;
             sutunBVeri = responseSutunB.Values;
             columnWordData = responseSearchWord.Values;
             columnKelimeVeri = responseAramaKelime.Values;
-
         }
 
-        public string KelimeAra(string AranacakWord)
+        public Tuple<List<string>, List<string>> KelimeAra(string AranacakWord)
         {
-            List<string> sonuc = new List<string>();
+            List<string> sonucA = new List<string>();
+            List<string> sonucB = new List<string>();
 
             if (columnWordData != null && columnWordData.Count > 0)
             {
@@ -95,59 +98,74 @@ namespace kelimeciniz
                 {
                     for (int j = 0; j < columnWordData[i].Count; j++)
                     {
-                        string[] words = columnWordData[i][j].ToString().Split(new[] { ' ', '-', '/' }, StringSplitOptions.RemoveEmptyEntries); 
+                        string[] words = columnWordData[i][j].ToString().Split(new[] { ' ', '-', '/' }, StringSplitOptions.RemoveEmptyEntries);
                         foreach (string word in words)
                         {
                             if (word.Trim().Equals(AranacakWord, StringComparison.OrdinalIgnoreCase))
                             {
-                                sonuc.Add(columnKelimeVeri[i][j].ToString());
+                                sonucA.Add(columnWordData[i][j].ToString());
+                                sonucB.Add(columnKelimeVeri[i][j].ToString());
                             }
                         }
                     }
                 }
             }
-            var yinelemeSil = new System.Collections.Generic.HashSet<string>(sonuc); //yinelemelri kaldırmak için.
+            else
+            {
+                sonucA.Add(kelimeDuzelt(AranacakWord));
+                sonucB.Add(EnglishToTurkish(AranacakWord).ToString());
+            }
 
-            return string.Join(", ", yinelemeSil);
+            var uniqueSonucA = new HashSet<string>(sonucA);
+            var uniqueSonucB = new HashSet<string>(sonucB);
+
+            return new Tuple<List<string>, List<string>>(uniqueSonucA.ToList(), uniqueSonucB.ToList());
+        }
+
+        private string kelimeDuzelt(string kelime)
+        {
+            string sonuc = char.ToUpper(kelime[0]) + kelime.Substring(1).ToLower();
+            return sonuc;
         }
 
         public void VeriEkle(string word, string kelime)
         {
-            // A ve B sütunlarının sonuna veri eklemek için hedef hücreyi belirleyin
-            var requestRange = $"Sayfa1!A:B";
+            string[] words = KelimeAra(word).Item1.ToArray();
+            string[] kelimeler = KelimeAra(word).Item2.ToArray();
 
-            // Var olan veriyi alın
-            SpreadsheetsResource.ValuesResource.GetRequest getRequest = service.Spreadsheets.Values.Get(spreadsheetId, requestRange);
-            ValueRange response = getRequest.Execute();
-            IList<IList<object>> values = response.Values;
-
-            // Yeni veriyi oluşturun
-            var newRow = new List<object> { word, kelime };
-
-            // Var olan veriye yeni veriyi ekleyin
-            if (values == null)
+            for (int i = 0; i < words.Length; i++)
+                if (word.ToLower() == words[i].ToLower())
+                    kelime = kelimeDuzelt(kelimeler[i]);//kelimeyi eklerken ilk harfini büyük diğer harfleri küçük şekilde ekliyor.
+                else
+                {
+                   kelime= EnglishToTurkish(word).ToString();
+                }
+            // Veriyi eklemek için gereken parametreleri oluştur
+            body = new ValueRange
             {
-                values = new List<IList<object>>();
-            }
-            values.Add(newRow);
+                Values = new List<IList<object>> { new List<object> { word, kelime } }
+            };
 
-            // Değişiklikleri kaydedin
-            ValueRange body = new ValueRange { Values = values };
-            SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest = service.Spreadsheets.Values.Update(body, spreadsheetId, requestRange);
-            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
-            UpdateValuesResponse responseUpdate = updateRequest.Execute();
+            verireq = service.Spreadsheets.Values.Append(body, spreadsheetId, range);//Veri ekleme
+
+            // Veriyi eklemek istediğiniz hücreyi belirleyin
+
+            // Veriyi ekle
+            verireq.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+            verireq.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+
+            AppendValuesResponse response = verireq.Execute();
 
             // İşlem sonucunu kontrol edin
-            if (responseUpdate.UpdatedCells > 0)
-            {
-                Console.WriteLine("Veri başarıyla eklendi.");//Değişebilir
-            }
-            else
-            {
-                Console.WriteLine("Veri ekleme başarısız.");
-            }
+            //if (responseUpdate.UpdatedCells > 0)
+            //{
+            //    Console.WriteLine("Veri başarıyla eklendi.");//Değişebilir
+            //}
+            //else
+            //{
+            //Console.WriteLine("Veri ekleme başarısız.");
+            //}
         }
-
 
         public Tuple<List<string>,List<string>> Sayfa1Veri()
         {
@@ -178,6 +196,29 @@ namespace kelimeciniz
 
             return veri;
         }
-  
+
+        static async Task<string> EnglishToTurkish(string text)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = $"https://mymemory.translated.net/api/get?q={text}&langpair=en|tr&key={myMemoryApiKey}";
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // JSON ayrıştırma ile "translatedText" alanını elde edebilirsiniz.
+                    dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+                    string translatedText = json.responseData.translatedText;
+
+                    return translatedText;
+                }
+                else
+                {
+                    return "Çevirisiz.";
+                }
+            }
+        }
+
     }
 }
